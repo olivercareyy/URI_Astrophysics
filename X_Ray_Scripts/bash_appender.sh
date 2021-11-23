1d0579200be88669c908ac29a564762a163ea070
Monster <erikvcarlson@gmail.com>
2021-11-23 14:09:23 -0500

    Change of File

--- X_Ray_Scripts/bash_appender.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1..100}
- do 
-    cat bird_test_data.csv >> test_data.csv
-done
---
+++ X_Ray_Scripts/example_x-ray_processing.sh
@@ -0,0 +1,1 @@
+dmcopy "acisf03111_repro_evt2.fits[energy=500:7000]" acisf03111_repro_evt2_energy_0.5-7000.fits 
